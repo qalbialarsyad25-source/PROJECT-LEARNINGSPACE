d8475fdc7a0abec6850989cff8ec47f6bfcc2727
Muhammad Qalbi Al Arsyad <qalbialarsyad25@gmail.com>
2026-05-21 11:36:07 +0700

    feat: add subjectGrade model

--- internal/entity/subjectGrade.go
+++ internal/entity/subjectGrade.go
@@ -6,5 +6,5 @@ type SubjectGrade struct {
 	Id           uuid.UUID `gorm:"type:varchar(35); primaryKey"`
 	Grade        float64   `gorm:"type:decimal(10, 2); not null"`
 	ReportCardId uuid.UUID `gorm:"type:char(36)"`
-	Subject      []Subject `gorm:"foreignKey;SubjectGradeId"`
+	SubjectId    uuid.UUID `gorm:"type:char(36)"`
 }
---
+++ internal/model/subjectGrade.go
@@ -0,0 +1,37 @@
+package model
+
+import (
+	"learningSpace/internal/entity"
+
+	"github.com/google/uuid"
+)
+
+type SubjectGradeResponse struct {
+	Id    uuid.UUID `json:"id"`
+	Grade float64   `json:"grade"`
+}
+
+type CreateSubjectGrade struct {
+	Grade float64 `json:"grade"`
+}
+
+type EditSubjectGrade struct {
+	Grade float64 `json:"grade"`
+}
+
+func ToSubjectGradeResponse(subjectGrade entity.SubjectGrade) SubjectGradeResponse {
+	return SubjectGradeResponse{
+		Id:    subjectGrade.Id,
+		Grade: subjectGrade.Grade,
+	}
+}
+
+func (p *EditSubjectGrade) ToMap() map[string]any {
+	Update := map[string]any{}
+
+	if p.Grade != 0 {
+		Update["grade"] = p.Grade
+	}
+
+	return Update
+}
