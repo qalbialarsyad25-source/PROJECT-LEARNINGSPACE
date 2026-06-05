eb8c10bfbdb7d4908a5b48e8f73f263bf9e49908
Muhammad Qalbi Al Arsyad <qalbialarsyad25@gmail.com>
2026-06-06 00:39:56 +0700

    refactor: userId nor null and constraint

--- internal/entity/presence.go
+++ internal/entity/presence.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Presence struct {
-	Id        uuid.UUID `gorm:"type:varchar(36); primaryKey"`
+	Id        uuid.UUID `gorm:"type:char(36); primaryKey"`
 	Presence  bool      `gorm:"default:false"`
 	Content   string    `gorm:"type:varchar(255)"`
 	Title     string    `gorm:"type:varchar(100); not null"`
 	CreatedAt time.Time `gorm:"type:timeStamp; not null; autoCreatedTime"`
-	UserId    uuid.UUID `gorm:"type:char(36)"`
+	UserId    uuid.UUID `gorm:"type:char(36);not null;contraint;OnDelete:CASCADE"`
 }
